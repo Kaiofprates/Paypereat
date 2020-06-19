315ec60a6ebc9daa25e2f7e5b9bfd637fd951746
Kelviny H <kelvinyhenrique17@gmail.com>
2020-06-19 13:51:06 -0300

    Building Order Detail

---
+++ src/Screens/OrderDetail/index.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { SafeAreaView, Text } from 'react-native';
+import styled from 'styled-components';
+import alfaceLg from '../../assets/Products/alfacelg.png';
+import shoppingCart from '../../assets/Icons/Cart/shopping-cart.png';
+
+export default function ProductDetail() {
+    return (
+        <SafeAreaView >
+    <Text>Olá</Text>
+        </SafeAreaView>
+    )
+}
--- src/navigators/OrdersStack.js
+++ src/navigators/OrdersStack.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import {Text, Button} from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Checkout from '../Screens/Checkout';
-import Cart from '../Screens/Cart';
+import OrderDetail from '../Screens/OrderDetail';
 import Orders from '../Screens/Orders';
 
 const OrdersStack = createStackNavigator();
@@ -11,6 +12,15 @@ export default () => {
         <OrdersStack.Navigator>
             <OrdersStack.Screen name="Pedidos" component={Orders} options={({ navigation }) => ({
                 title: 'Pedidos',
+                headerTitleAlign: 'center',
+                headerRight: () => (
+                     <Button title="Order Detail" onPress={() => {
+                        navigation.navigate('OrderDetail');
+                     }}/>
+                )
+            })} />
+            <OrdersStack.Screen name="OrderDetail" component={OrderDetail} options={({ navigation }) => ({
+                title: 'Order Detail',
                 headerTitleAlign: 'center',
                 headerLeft: null,
             })} />
